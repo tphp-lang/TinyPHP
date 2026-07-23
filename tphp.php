c8be9196fa7c37bbb02420fe94b0c9022117d18d
KingBes <48203677+KingBes@users.noreply.github.com>
2026-07-23 21:17:09 +0800

    Update tphp.php

--- tphp.php
+++ tphp.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 // ============================================================
 
 /** TinyPHP 版本号 */
-const TPHP_VERSION = '0.2.0-beta.3';
+const TPHP_VERSION = '0.2.0-beta.4';
 
 spl_autoload_register(function (string $class): void {
     $baseDir = __DIR__ . '/src';
